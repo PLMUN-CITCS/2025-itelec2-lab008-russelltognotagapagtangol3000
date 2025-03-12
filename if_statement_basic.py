d9347a3be3d76eacca4bfbda62bcf47a26044b7a
zach <tognorussell@gmail.com>
2025-03-12 12:12:15 +0800

    Submitting Python Week 04 - Laboratory # 08

---
+++ if_statement_basic.py
@@ -0,0 +1,6 @@
+# Define a variable for temperature
+temperature = 35  # Change this value to test different cases
+
+# If statement to check if temperature is greater than 30
+if temperature > 30:
+    print("It's a hot day!")  # Indented code runs only if condition is True
